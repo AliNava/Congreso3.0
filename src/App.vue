fcb456b51ffb9b2c92afdacbc44dd3de0ee2ab48
AliNava <alien.nava.alfaro@gmail.com>
2018-07-31 09:14:26 -0500

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light " >
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <img src="../assets/logocongreso.png" class="float-left" style="max-width:200px "/>
+      <ul class="navbar-nav" >
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            LXIII Legislatura
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Conoce a tu diputado</a>
+            <a class="dropdown-item" href="#">Fracciones Parlamentarias</a>
+            <a class="dropdown-item" href="#">Composición Gráfica</a>
+            <a class="dropdown-item" href="#">Mesa Directiva </a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Congreso del estado
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink1">
+            <a class="dropdown-item" href="#">¿Qué es el congreso del estado?</a>
+            <a class="dropdown-item" href="#">Historia del congreso</a>
+            <a class="dropdown-item" href="#">Muro de Honor</a>
+            <a class="dropdown-item" href="#">Biblioteca</a>
+            <a class="dropdown-item" href="#">Salón de Sesiones Plenarias</a>
+            <a class="dropdown-item" href="#">Salón legisladoras</a>
+            <a class="dropdown-item" href="#">Legislaturas</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Actividad Legislativa
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
+            <a class="dropdown-item" href="#">Iniciativas</a>
+            <a class="dropdown-item" href="#">Puntos de acuerdo</a>
+            <a class="dropdown-item" href="#">Dictamenes</a>
+            <a class="dropdown-item" href="#">Acuerdos Legislativos</a>
+            <a class="dropdown-item" href="#">Informe de Acividades </a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Transparencia
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink3">
+            <a class="dropdown-item" href="#">Artículoo 55</a>
+            <a class="dropdown-item" href="#">Artículo 57</a>
+            <a class="dropdown-item" href="#">Transparencia anterior</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Prensa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">IIL</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -55,4 +86,8 @@ li {
 a {
   color: #42b983;
 }
+navbar-nav{
+  margin-top: 50px;
+  }
+
 </style>
