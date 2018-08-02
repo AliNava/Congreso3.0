192feb81c4518f72a47ed5a9de783c35285fe1ac
AliNava <alien.nava.alfaro@gmail.com>
2018-08-01 19:03:51 -0500

    se utilizo router

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import 'popper.js/dist/esm/popper.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
+    router,
   render: h => h(App)
 }).$mount('#app')
